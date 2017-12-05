f13fe4de0e27d91857f8f44e6fdbcde96f547b11
thedavidkruse <thedavidkruse@gmail.com>
2017-12-04 18:33:38 -0700

    didnt work

--- src/components/coinTable/Coin.js
+++ src/components/coinTable/Coin.js
@@ -11,7 +11,7 @@ class Coin extends Component {
     }
 
     componentDidMount(){
-        const cryptoSocket = io.connect('https://coinzy-alpha.herokuapp.com/', {path: '/grape'});
+        const cryptoSocket = io.connect('https://coinzy-alpha.herokuapp.com/', {path: '/grape', transport: ['websocket']});
         cryptoSocket.on('trades', (tradeMsg) => {
             console.log('got message')
             if (this.props.coinData.short === tradeMsg.short){
