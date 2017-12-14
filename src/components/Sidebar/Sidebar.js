691fa76df8ce58497c09b408846f98864863b6c4
thedavidkruse <thedavidkruse@gmail.com>
2017-12-14 12:33:21 -0700

    Added sidebar

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,21 @@
+// @flow
+
+import React, { Component } from 'react';
+
+class Sidebar extends Component<null, null> {
+  render () {
+    return (
+      <div style={stylesheet}>
+          <p>
+              Hello, I am a sidebar.
+          </p>
+      </div>
+    )
+  }
+}
+
+let stylesheet = {
+    backgroundColor: 'red'
+}
+
+export default Sidebar
